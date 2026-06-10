474231d049b20d8d9bd655d605e4ff90026e0181
tarcisio queiroz at <tarcisio.queiroz.atan@gmail.com>
2026-06-10 16:27:57 -0300

    FEAT: Model Package Previews

---
+++ locadora/model/Cliente.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Cliente {
+
+    private Integer codCliente;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String endereco;
+    private Date dataNascimento;
+    private ArrayList<Item> itens = new ArrayList<Item>();
+
+}
